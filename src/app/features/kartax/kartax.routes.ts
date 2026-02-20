e2f3b6e5019326f6881741fc7deda91ed1d14685
Francisco Carmona <flcarmonac@yahoo.com>
2026-02-20 03:06:19 -0300

    Update

--- src/app/features/kartax/kartax.routes.ts
+++ src/app/features/kartax/kartax.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { MobilePage } from "@features/kartax/pages/mobile-page/mobile-page";
+import { LayoutMobile } from "@layouts/layout-mobile/layout-mobile";
 
 export const KARTAX_ROUTES: Routes = [
   {
